97c1b0d2ca8916d6ca712b6d610caa92e272471c
ShreyaaaJoshi <shreyajoshi1510@gmail.com>
2026-04-22 14:11:22 +0530

    fix: use correct env variable name VITE_BACKEND_URL

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,8 +1,5 @@
 const getBaseUrl = () => {
-    if(import.meta.env.VITE_API_URL) {
-        return import.meta.env.VITE_API_URL  
-    }
-    return "http://localhost:5000"
+    return import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
 }
 
 export default getBaseUrl;
