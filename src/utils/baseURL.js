8a3247c9b413b42c80bd05774327a44eb77ab7c5
ShreyaaaJoshi <shreyajoshi1510@gmail.com>
2026-04-22 14:02:00 +0530

    fix: remove trailing slash from base URL

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,8 @@
 const getBaseUrl = () => {
-    return  "https://inventrabook-backend.onrender.com/" || "http://localhost:5000" 
+    if(import.meta.env.VITE_API_URL) {
+        return import.meta.env.VITE_API_URL  
+    }
+    return "http://localhost:5000"
 }
 
 export default getBaseUrl;
